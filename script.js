8cf7e6bb6e1e14918e8ea190dd7180d67eca9b8a
Ronny Olivares <ryogindieinc@gmail.com>
2025-08-03 12:02:49 -0500

    Se corrige código JS y se añade interactividad a la descripción usando JS para manipular el DOM

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// Selecionar el botón y el párrafo
 let botonSaludo = document.getElementById('btn-saludo');
+let parrafoDescripcion = document.getElementById('descripcion-perfil');
 
-function cambiarTextoBoton() {
-    botonSaludo.innerText = '¡Hola! Bienvenido a mi página de presentación. Ahora soy un experto en HTML, CSS y JavaScript básico.';
+// Define la función que cambiará el texto del párrafo
+function cambiarDescripcion() {
+    parrafoDescripcion.textContent = '¡Hola! Bienvenido a mi página de presentación. Ahora soy un experto en HTML, CSS y JavaScript básico.';
 }
 
-botonSaludo.addEventListener('click', cambiarTextoBoton);
+// Asignar el evento: cuando se haga clic en el botón, se ejecuta la función.
+botonSaludo.addEventListener('click', cambiarDescripcion);
